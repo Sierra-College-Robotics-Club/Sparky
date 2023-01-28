9e0e0fb8b83c1d0995c4f6722cbe4319823adbc1
John <myschoolacountsacount@gmail.com>
2023-01-28 14:33:32 -0800

    initial commit

---
+++ Arduino_Sensor_Sketches/Arduino_Sensor_Sketches.ino
@@ -0,0 +1,12 @@
+// This is code for testing the X001T, KY 026 and old firebot flame sensor
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
